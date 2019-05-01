218adefd38bda97cb21111269ce5496a4d2b6c8c
Brandon Aguirre <brag4918@colorado.edu>
2019-05-01 14:29:13 -0600

    modify assign points function

--- src/Helpers.py
+++ src/Helpers.py
@@ -1,24 +1,130 @@
 from os.path import join, isfile
 from pybaseball import statcast
 import pandas as pd
+import numpy as np
 
 def get_statcast_data(directory, year):
-    year = str(year)
-    data_path = join(directory, 'statcast_data_{}.csv'.format(year))
-    sc_data = None
-    if not isfile(data_path):
-        date_ranges = [
-            ('$-01-01', '$-03-31'),
-            ('2018-04-01', '$-06-30'),
-            ('$-07-01', '$-09-30'),
-            ('$-10-01', '$-12-31')
-        ]
-        date_ranges = [(date[0].replace('$', year), date[1].replace('$', year)) for date in date_ranges]
-        data = []
-        for dr in date_ranges:
-            data.append(statcast(start_dt=dr[0], end_dt=dr[1]))
-        sc_data = pd.concat(data, ignore_index=True)
-        sc_data.to_csv(data_path)
+  year = str(year)
+  data_path = join(directory, 'statcast_data_{}.csv'.format(year))
+  sc_data = None
+  if not isfile(data_path):
+    date_ranges = [
+      ('$-01-01', '$-03-31'),
+      ('2018-04-01', '$-06-30'),
+      ('$-07-01', '$-09-30'),
+      ('$-10-01', '$-12-31')
+    ]
+    date_ranges = [(date[0].replace('$', year), date[1].replace('$', year)) for date in date_ranges]
+    data = []
+    for dr in date_ranges:
+      data.append(statcast(start_dt=dr[0], end_dt=dr[1]))
+    sc_data = pd.concat(data, ignore_index=True)
+    sc_data.to_csv(data_path)
+  else:
+    sc_data = pd.read_csv(data_path, index_col=0)
+  return sc_data.copy()
+
+def event_map(e):
+  """
+  Map a string from the statcast database events column to a key
+  @param e str which is a value from the events column
+  @return str|int (h, hr, k) | -1
+  """
+  emap = {
+    'single': 'h',
+    'double': 'h',
+    'triple': 'h',
+    'home_run': 'hr',
+    'strikeout': 'k'
+  }
+  if e in emap:
+    return emap[e]
+  else:
+    return -1
+
+def get_proba_function(df):
+  """
+  Create a function which produces the probability of: a hit (excluding homerun), homerun, and strike
+  @param df pandas.DataFrame which has a years seasons worth of statcast data
+  @return a function which takes in an event (h, hr, k), and returns a probability of the event
+  """
+  n_1b = len(df.loc[df.events == 'single'])
+  n_2b = len(df.loc[df.events == 'double'])
+  n_3b = len(df.loc[df.events == 'triple'])
+  n_hr = len(df.loc[df.events == 'home_run'])
+  n_so = len(df.loc[df.events == 'strikeout'])
+  n_bb = len(df.loc[df.events == 'walk'])
+  emap = { 'h': n_1b + n_2b + n_3b, 'hr': n_hr, 'k': n_so }
+  return lambda e: emap[e] / sum(list(emap.values()))
+
+def wager(p, b, k):
+  """
+  Calculates the points a pitcher, and a batter should wager on a given event
+  @param p float: score of pitcher
+  @param b float: score of batter
+  @param k int: wager factor, weights how much players should wager
+  @return touple (float, float): (pitchers wager, batters wager)
+  """
+  ep = (10**(p/400))/((10**(p/400)) + (10**(b/400)))
+  eb = 1-ep
+  return (round(k(p)*ep, 3), round(k(b)*eb, 3))
+
+def get_assign_points_function(pf):
+  """
+  @param pf float: probability function of an event
+  @return function which takes (p, b, e, f) as args
+  """
+  def assign_points_function(p, b, e, k):
+    """
+    @param p float: score of pitcher
+    @param b float: score of batter
+    @param e str[] | str: event (h | hr | k)
+    @param k int: wager factor, weights how much players should wager
+    @return tuple (float, float) which contains (pitchers new points, batters new points)
+    """
+    pw, bw = wager(p, b, k)
+    if type(e) == str:
+      event = e
     else:
-        sc_data = pd.read_csv(data_path, index_col=0)
-    return sc_data.copy()
+      event = e.pop(0)
+    proba = pf(event)
+    points = (None, None)
+    if event == 'k':
+      percent = 1 - (proba)
+      points = (p+(bw*percent), b-(bw*percent))
+    elif event == 'h':
+      percent = (1 - (pf('h') + pf('hr')))/(pf('h')/pf('hr'))
+      points = (p-(bw*percent), b+(bw*percent))
+    elif event == 'hr':
+      percent = (1 - (pf('h') + pf('hr')))*(pf('h')/pf('hr'))
+      points = (p-(bw*percent), b+(bw*percent))
+    points = (round(points[0], 3), round(points[1], 3))
+    if type(e) == str or len(e) == 0:
+      return points
+    else:
+      return assign_points_function(points[0], points[1], e, f)
+  return assign_points_function
+
+def k_function(p):
+  """
+  @param p float which is the number of points a player has
+  @ return float which is the k multiplier for how much a player should wager
+  """
+  res = np.exp(3600/(p))
+  if res >= 20:
+    return 20*2
+  elif res <= 10:
+    return 10*2
+  else:
+    return res*2
+
+def history_avg(arr, n):
+  """
+  @param arr list which contains floats 
+  @param n int specifying how far back to average (window)
+  @return average from window length n to end of arr
+  """
+  l = len(arr)
+  if n > l:
+    n = l
+  return np.mean(arr[-n:])
--- src/create_app_data.py
+++ src/create_app_data.py
@@ -1,19 +1,137 @@
 import json
 import pandas as pd
+import numpy as np
 from os.path import join
+from Helpers import *
+from tqdm import tqdm
+import sys
 
-def main():
-  data = '../data'
-  file = 'master.csv'
-  df = pd.read_csv(join(data, file), encoding='latin-1')
+cmap = {
+  'y': '#fef160',
+  'g': '#2ecc71',
+  'b': '#22a7f0',
+  'r': '#f62459',
+}
+  
+e_style = {
+  'p': {
+    'k': 'point {{ fill-color: {} }}'.format(cmap['g']),
+    'h': 'point {{ fill-color: {} }}'.format(cmap['y']),
+    'hr': 'point {{ fill-color: {} }}'.format(cmap['r'])
+  },
+  'b': {
+    'k': 'point {{ fill-color: {} }}'.format(cmap['r']),
+    'h': 'point {{ fill-color: {} }}'.format(cmap['b']),
+    'hr': 'point {{ fill-color: {} }}'.format(cmap['g'])
+  }
+}
+
+def event_style(e, pb):
+  return e_style[pb][e]
+
+def export_mjs_module(outfile, data):
+  out = 'export default ' + json.dumps(data)
+  with open(outfile, 'w') as file:
+    file.write(out)
+
+def get_player_map(unique_ids, infile):
+  # needs to remove players who did not play in 2018
+  df = pd.read_csv(infile, encoding='latin-1')
   player_map = {}
 
   for row in df.iterrows():
     row = row[1]
-    player_map[row.mlb_name] = row.mlb_id
+    mlb_id = int(float(row.mlb_id))
+    if mlb_id in unique_ids:
+      player_map[mlb_id] = row.mlb_name
 
-  with open('../docs/data/player_map.json', 'w') as file:
-    json.dump(player_map, file)
+  class PMap:
+    def __init__(self, pm):
+      self.map = pm
+    def get(self, pid):
+      if pid in self.map:
+        return self.map[pid]
+      else:
+        return str(pid)
+    def getadd(self, pid):
+      if pid in self.map:
+        return self.map[pid]
+      else:
+        self.map[pid] = str(pid)
+        return str(pid)
+
+  return PMap(player_map)
+
+def create_player_data(data_folder, out_folder, year):
+  print('loading data...')
+  data = get_statcast_data(data_folder, year)
+  print('finished loading data...')
+  # factor to weight how much points should be wagered in an event
+  wf = k_function
+  # event probability function
+  epf = get_proba_function(data)
+  # assign points function
+  update_score = get_assign_points_function(epf)
+  # history: how far back to avg a score (prevents sensitivity to local events)
+  h = 5
+
+  unique_ids = list(map(int, list(set(list(data.pitcher.unique()) + list(data.batter.unique())))))
+  player_map = get_player_map(unique_ids, join(data_folder, 'master.csv'))
+
+  point_df = data.loc[:, ('game_date', 'game_pk', 'sv_id', 'pitcher', 'batter', 'events')].copy()
+  points_df = point_df.sort_values(['game_date', 'sv_id'])
+  # index, ranking, event_type/against, style
+  pitchers_cdata = { int(p): [[0, 1500.0, 'Initialized', 'point {fill-color: #00FF00}']]
+              for p in point_df.pitcher.unique()}
+  batters_cdata = { int(b): [[0, 1500.0, 'Initialized', 'point {fill-color: #00FF00}']]
+             for b in point_df.batter.unique()}
+  pitchers = { int(p): [1500.0]
+              for p in point_df.pitcher.unique()}
+  batters = { int(b): [1500.0]
+             for b in point_df.batter.unique()}
+
+  for row in tqdm(point_df.iterrows()):
+    row = row[1]
+    event = event_map(row.events)
+    if event != -1:
+      p_id = int(row.pitcher)
+      b_id = int(row.batter)
+      update = update_score(history_avg(pitchers[p_id], h), history_avg(batters[b_id], h), event, wf)
+      if p_id not in unique_ids:
+        print('p_id: ', p_id)
+      if b_id not in unique_ids:
+        print('b_id: ', b_id)
+      pitchers_cdata[p_id].append([
+        len(pitchers_cdata[p_id]),
+        update[0],
+        row.events + '\nAgainst: ' + player_map.getadd(b_id),
+        event_style(event, 'p')
+      ])
+      batters_cdata[b_id].append([
+        len(batters_cdata[b_id]),
+        update[1],
+        row.events + '\nAgainst: ' + player_map.getadd(p_id),
+        event_style(event, 'b')
+      ])
+      pitchers[p_id].append(update[0])
+      batters[b_id].append(update[1])
+
+  export_mjs_module(join(out_folder, 'batters-{}.mjs'.format(year)), batters_cdata)
+  export_mjs_module(join(out_folder, 'pitchers-{}.mjs'.format(year)), pitchers_cdata)
+  export_mjs_module(join(out_folder, 'player-map-{}.mjs'.format(year)), player_map.map)
+
+def main():
+  if len(sys.argv) < 2:
+    print('usage: python3 create_app_data.py int:year')
+    exit(1)
+  year = 0
+  try:
+    year = int(float(sys.argv[1]))
+  except:
+    print('usage: python3 create_app_data.py int:year')
+    exit(1)
+
+  create_player_data('../data', '../docs/data', year)
 
 if __name__ == '__main__':
   main()
